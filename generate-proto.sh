2bbe0a4fcd44c07bcb06e55b377d17e25d42f0d3
Louis Chaumier <louischaumier78@gmail.com>
2025-06-16 16:03:35 +0200

    remove: proto/grpc generated files

--- generate-proto.sh
+++ generate-proto.sh
@@ -1,5 +1,7 @@
-rm -f ./server/generated/*
-rm -f ./client/generated/*
+rm -rf ./server/generated
+rm -rf ./client/generated
+mkdir ./client/generated
+mkdir ./server/generated
 protoc --go_out=./server/ --go-grpc_out=./server/ -I./proto ./proto/file.proto
 protoc --go_out=./client/ --go-grpc_out=./client/ -I./proto ./proto/file.proto
 protoc --go_out=./server/ --go-grpc_out=./server/ -I./proto ./proto/dir.proto
